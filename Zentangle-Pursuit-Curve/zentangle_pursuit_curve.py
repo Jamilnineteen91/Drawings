94652cc2c6bf97c57826289ebb2ac0e3ea28f8db
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-01-20 15:38:56 -0600

    created setup, pursuit curve un-solved

---
+++ Zentangle-Pursuit-Curve/zentangle_pursuit_curve.py
@@ -0,0 +1,17 @@
+import turtle
+
+#Setup
+t=turtle.Turtle()
+t.speed(9)
+LoadWindow=turtle.Screen()
+LoadWindow.setup(width=1000,height=1000)
+LoadWindow.title('Zentangle Pursuit Curve')
+
+
+
+
+
+
+
+
+LoadWindow.exitonclick()
